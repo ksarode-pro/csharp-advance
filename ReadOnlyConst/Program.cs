549d817e802596404a939dea6a4ee22a7d401033
Kiran Sarode <ksarode.pro@gmail.com>
2026-04-25 10:12:12 +0530

    const and readonly

--- ReadOnlyConst/Program.cs
+++ ReadOnlyConst/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.IO.Pipes;
+
+namespace ReadOnlyConst
+{
+    public class Program
+    {
+        //not allowed as a local variable; only field
+         //Runtime constant, value is defined either with declaration or in constructor defination
+        //usecase: confidurations that may change
+        private readonly string  _connectionString;
+        
+        public Program(string conn)
+        {
+            this._connectionString = conn;
+        }
+        
+        //usecase: universal truth that will neven change
+        //compile time constants
+        //value must be defined inline with declaration
+        //implicitly static
+        const float PI = 3.14;
+            
+        public static void Main(String[] args)
+        {
+            
+            //allowed as local variable and field
+            const float DAYS_IN_WEEK = 3.14;
+
+            //compilor replaces PI with 3.14 at compile time
+            //eg Console.WriteLine(3.14);
+            System.Console.WriteLine(PI);
+
+            Program p = new Program("server=localhost;database=Study;username=sa;password=Testing@123;TrustedConnection=true;");
+            System.Console.WriteLine(p._connectionString);
+        }
+    }
+}
