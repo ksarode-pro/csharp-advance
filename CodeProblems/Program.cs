d678085deb1e5ac0527896ab1db1d0b453a43b53
Kiran Sarode <ksarode.pro@gmail.com>
2026-04-26 11:30:44 +0530

    more methods added to reverse

--- CodeProblems/Program.cs
+++ CodeProblems/Program.cs
@@ -8,4 +8,6 @@ Console.WriteLine($"Factorial of 5 is " + factorial.FactorialRecurssive(5));
 ReverseString reverse = new ReverseString();
 reverse.ReverseStringNormal("Kiran");
 reverse.ReverseStringWithoutInBluildFunctions("Kiran");
+reverse.ReverseStringWithStringBuilder("Kiran");
+reverse.ReverseStringWithTwoWayPointers("Kiran");
 Console.WriteLine($"Reverse of Kiran is " + reverse.ReverseStringRecurssive("Kiran"));
--- CodeProblems/ReverseString.cs
+++ CodeProblems/ReverseString.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace CodeProblems
 {
@@ -20,6 +21,33 @@ namespace CodeProblems
             }
 
             System.Console.WriteLine($"Reverse of {s} is {new string(revChars)}");
+        }
+
+        internal void ReverseStringWithStringBuilder(string s)
+        {
+            StringBuilder revChars = new StringBuilder();
+            for(int i = (s.Length - 1); i >= 0; i--)
+            {
+                revChars.Append(s[i]);
+            }
+
+            System.Console.WriteLine($"Reverse of {s} is {revChars.ToString()}");
+        }
+
+        internal void ReverseStringWithTwoWayPointers(string str)
+        {
+            // char temp;
+            StringBuilder s = new StringBuilder(str);
+            for(int start = 0, end = (s.Length - 1); start < end; start++, end--)
+            {
+                //used tuple to eliiminate temp varibale
+                (s[start],s[end]) = (s[end],s[start]);
+                // temp = s[start];
+                // s[start] = s[end];
+                // s[end] = temp;   
+            }
+
+            System.Console.WriteLine($"Reverse is {s}");
         }
 
         internal string ReverseStringRecurssive(string s)
