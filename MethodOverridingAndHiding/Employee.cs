ea04d2b4ce44dae4293830334bc6dfd164019aad
Kiran Sarode <ksarode.pro@gmail.com>
2026-04-27 19:32:39 +0530

    additional concept added

--- MethodOverridingAndHiding/Employee.cs
+++ MethodOverridingAndHiding/Employee.cs
@@ -2,18 +2,43 @@ namespace MethosOverridingAndHiding
 {
     class BaseClass
     {
+        //public <NO virtual KEYWORD> void PrintFullName() //error CS0506: 'DerivedClass.PrintFullName()': cannot override inherited member 'BaseClass.PrintFullName()' because it is not marked virtual, abstract, or override
         public virtual void PrintFullName()
         {
             System.Console.WriteLine("Base Class PrintFullName Method");
+        }
+
+        public virtual void GetAddress()
+        {
+            System.Console.WriteLine("Base Class GetAddress Method");
+        }
+
+        public virtual void GetSalary() //marked as virtual but no presence in derived class, no error, 
+        {
+            System.Console.WriteLine("Base Class GetAddress Method");
         }
     }
 
     class DerivedClass : BaseClass
     {
-        //public override void PrintFullName()
-        public new void PrintFullName()
+        public override void PrintFullName()
         {
             System.Console.WriteLine("Derived Class PrintFullName Method");
         }
+
+        //public void GetAddress() //NO override KEYWORD, hinding with warning. no error
+        public new void GetAddress()
+        {
+            System.Console.WriteLine("Derived Class GetAddress Method");
+        }
+
+        /*
+        //Marked as virtual in base, but no presence in derived class (no hiding, no override) produces no error/warning. 
+        // 
+        public virtual void GetSalary() 
+        {
+            System.Console.WriteLine("Base Class GetAddress Method");
+        }
+        */
     }
 }
--- MethodOverridingAndHiding/Program.cs
+++ MethodOverridingAndHiding/Program.cs
@@ -19,7 +19,15 @@ o3.PrintFullName();
 
 //In case of new keyword (method hiding), the method to be called 
 // is determined by the reference type and not by the object type.
-//
+
 // In case of virtual-override, the method to be called 
 // is determined by the object type and not by the reference type.
 
+
+BaseClass o4 = new DerivedClass();
+o4.GetAddress(); //method hiding with 'new' keyword. Reference is considered while choosing method to execute.
+
+
+//method is virtual but not overrriden, base class's inherited version is called.
+DerivedClass dobj = new DerivedClass();
+dobj.GetSalary(); 
