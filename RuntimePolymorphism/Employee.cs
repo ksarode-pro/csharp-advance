85626d9e591a3ae01d36f5a81b928062ebddebc6
Kiran Sarode <ksarode.pro@gmail.com>
2026-04-27 19:34:47 +0530

    project name chnaged

--- RuntimePolymorphism/Employee.cs
+++ RuntimePolymorphism/Employee.cs
@@ -1,4 +1,4 @@
-namespace MethosOverridingAndHiding
+namespace RuntimePolymorphism
 {
     class BaseClass
     {
--- RuntimePolymorphism/Program.cs
+++ RuntimePolymorphism/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using MethosOverridingAndHiding;
+using RuntimePolymorphism;
 
 DerivedClass o = new DerivedClass();
 o.PrintFullName();
